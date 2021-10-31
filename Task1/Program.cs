e970f3d8f8c7b3bd2f91fac8aad62b3ce87e444a
Selebry <murad1703@yandex.ru>
2021-10-31 19:20:49 +0300

    Вроде бы финальный вариант

---
+++ Task1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+/*
+Условие На стандартный вход подается строка, содержащая слова, разделенные пробелами. 
+Необходимо сделать заглавной буквой начало каждого слова в строке. Вывести на стандартный вывод
+Примеры Входные данные: hEllo world!
+Выходные данные: Hello World!
+*/
+namespace Task1
+{
+    class Program
+    {
+        static string Capitalize(string s)
+        {
+            var arrayChar = s
+                .ToLowerInvariant()
+                .ToCharArray();
+            arrayChar[0] = char.ToUpper(arrayChar[0]);
+            return new string(arrayChar);
+        }
+
+        static void Main()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            string InputLine = Console.ReadLine();
+            var words = InputLine
+                .Split(' ')
+                .Where(word => !String.IsNullOrWhiteSpace(word))
+                .Select(word => Capitalize(word))
+                .ToArray();
+            InputLine = null;
+            foreach (var word in words)
+            {
+                InputLine += word + ' ';
+            }
+
+            Console.WriteLine(InputLine);
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+/*
+На стандартный вход подается строка. Нужно написать функцию, которая на вход принимает строку, а на выходе мы получаем строку, состоящую из цифр, 
+которые соответствуют правилу: Если текущий символ ни разу еще не встречался, то пишем 1, 
+если он уже встречался ранее, то записываем сколько раз он уже встречался Вывести на стандартный вывод
+Примеры Входные данные: Привет мир!
+Выходные данные: 11111111221
+ */
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.Write("Введите строку: ");
+            string InputLine = Console.ReadLine();
+            var charInNumber = InputLine
+                .ToCharArray()
+                .Distinct()
+                .ToDictionary(word => word, word => 1);
+            string numbers=null;
+            foreach (var s in InputLine)
+            {
+                numbers += charInNumber[s];
+                charInNumber[s]++;
+            }
+            Console.WriteLine(numbers);
+        }
+    }
+}
